900fcc542a103c41ddacd68cfb3040cf05e85420
Kaloyan <kaloyankulov2003kk@gmail.com>
2023-10-05 13:28:07 +0300

    Created pages

---
+++ FitFusion/FitFusionDesktop/Pages/Login.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FitFusionDesktop.Pages
+{
+    public partial class Login : Form
+    {
+        public event EventHandler ButtonClicked;
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (txtEmail.Text == "admin")
+            {
+                OnButtonClicked(EventArgs.Empty);
+            } 
+        }
+        protected virtual void OnButtonClicked(EventArgs e)
+        {
+            ButtonClicked?.Invoke(this, e);
+        }
+
+    }
+}
