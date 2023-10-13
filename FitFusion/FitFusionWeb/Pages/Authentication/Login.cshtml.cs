a82a78347fef3181a6cde9b0aead11de24191d31
Kaloyan <kaloyankulov2003kk@gmail.com>
2023-10-14 00:52:15 +0300

    Authentication added

---
+++ FitFusion/FitFusionWeb/Pages/Authentication/Login.cshtml.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Models.User;
+using System.Security.Claims;
+
+namespace FitFusionWeb.Pages.Authentication
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public Owner owner { get; set; } = new();
+
+        public IActionResult OnGet()
+        {
+
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToPage("../Account");
+            }
+
+            owner.FirstName = "Byron";
+            owner.Password = "manfred";
+
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (owner.FirstName == "Byron")
+            {
+                List<Claim> claims = new List<Claim>();
+                claims.Add(new Claim(ClaimTypes.Name, owner.FirstName));
+                claims.Add(new Claim("id", "1"));
+
+                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                HttpContext.SignInAsync(new ClaimsPrincipal(claimsIdentity));
+
+                return RedirectToPage("../Index");
+            }
+
+            return Page();
+        }
+
+    }
+}
--- FitFusion/FitFusionWeb/Program.cs
+++ FitFusion/FitFusionWeb/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options => {
+        options.LoginPath = new PathString("/Authentification/Login");
+        options.AccessDeniedPath = new PathString("/AccessDenied");
+    });
 
 var app = builder.Build();
 
@@ -13,11 +21,19 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+
+//public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+//{
+//    app.UseAuthentication();
+//    app.UseAuthorization();
+//}
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
