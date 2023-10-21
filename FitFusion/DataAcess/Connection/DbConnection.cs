f31ae241deafeefc99f4afbf908e62720d4e23ba
Kaloyan <kaloyankulov2003kk@gmail.com>
2023-10-21 16:26:55 +0300

    Connection accepting multiple requests change

--- FitFusion/DataAcess/Connection/DbConnection.cs
+++ FitFusion/DataAcess/Connection/DbConnection.cs
@@ -9,6 +9,6 @@ namespace DataAcess.Connection
     public class DbConnection
     {
         // TODO: Need to change the string
-        public static string ConnectionString { get; } = "Data Source=mssqlstud.fhict.local;Initial Catalog=dbi500862;User ID=dbi500862;Password=Mkw4PQHJeU;";
+        public static string ConnectionString { get; } = "Data Source=mssqlstud.fhict.local;Initial Catalog=dbi500862;User ID=dbi500862;Password=Mkw4PQHJeU;MultipleActiveResultSets=True;";
     }
 }
