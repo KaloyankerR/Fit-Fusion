c9a9ec37284d041bb80be07534a5edcc1794fe34
Kaloyan <kaloyankulov2003kk@gmail.com>
2023-10-29 21:06:58 +0200

    Update web page

--- FitFusion/FitFusionWeb/Pages/Products/Update.cshtml.cs
+++ FitFusion/FitFusionWeb/Pages/Products/Update.cshtml.cs
@@ -9,12 +9,31 @@ namespace FitFusionWeb.Pages.Products
     {
         [BindProperty(SupportsGet = true)]
         public int Id { get; set; }
+        [BindProperty]
         public Product Product { get; set; } = new Product();
         private readonly ProductManager _productManager = new ProductManager(new DataAcess.ProductDAO());
 
         public void OnGet()
         {
             Product = _productManager.GetProductById(Id);
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["ErrorMessage"] = "Please check the fields again!";
+                return Page();
+            }
+
+            if (_productManager.UpdateProduct(Product)) 
+            {
+                TempData["ErrorMessage"] = "Product successfully updated!";
+                return RedirectToPage("./All");
+            }
+
+            TempData["ErrorMessage"] = "An error occured!";
+            return RedirectToPage("./All");
         }
 
     }
