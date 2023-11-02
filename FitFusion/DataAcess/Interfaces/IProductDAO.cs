fc28d53bce81c910754185bb01df419b6b3d6414
Kaloyan <kaloyankulov2003kk@gmail.com>
2023-11-02 17:03:39 +0200

    Created Interfaces

---
+++ FitFusion/DataAcess/Interfaces/IProductDAO.cs
@@ -0,0 +1,18 @@
+﻿using Models.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcess.Interfaces
+{
+    public interface IProductDAO
+    {
+        void CreateProduct(Product product);
+        void UpdateProduct(Product updatedProduct);
+        void DeleteProduct(int productId);
+        Product GetProductById(int productId);
+        List<Product> GetAllProducts();
+    }
+}
