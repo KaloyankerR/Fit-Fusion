a4d80a372dbf5190b508d3d27a4b649f9ff9274c
Kaloyan <kaloyankulov2003kk@gmail.com>
2023-11-02 17:04:49 +0200

    Started unit testing

---
+++ FitFusion/FitFusionTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace FitFusionTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
