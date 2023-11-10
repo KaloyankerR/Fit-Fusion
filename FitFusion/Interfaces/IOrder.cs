24f72277c75ed39d48aa5229cc771a3e20922f9b
Kaloyan <kaloyankulov2003kk@gmail.com>
2023-11-10 20:35:12 +0100

    WEb page feedback

---
+++ FitFusion/Interfaces/IOrder.cs
@@ -0,0 +1,16 @@
+﻿using Models.Order;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public interface IOrder
+    {
+        bool CreateOrder(Order order);
+        Order GetOrder(int id);
+        List<Order> GetOrders();
+    }
+}
