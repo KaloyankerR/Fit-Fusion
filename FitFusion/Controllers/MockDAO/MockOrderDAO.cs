e3f1a599875c0a77a748b43a6a5107150023a4a1
Kaloyan <kaloyankulov2003kk@gmail.com>
2023-11-16 23:55:02 +0100

    Mock classes

---
+++ FitFusion/Controllers/MockDAO/MockOrderDAO.cs
@@ -0,0 +1,83 @@
+﻿using Models.Order;
+using ProductModel = Models.Product.Product;
+using Models.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models.Product;
+using Interfaces;
+
+namespace Services.MockDAO
+{
+    public class MockOrderDAO : IOrder
+    {
+        private readonly List<Order> orders;
+
+        public MockOrderDAO()
+        {
+            orders = new List<Order>()
+            {
+                new Order
+                {
+                    Id = 1,
+                    OrderDate = DateTime.Now.AddDays(-2),
+                    Customer = new Customer { Id = 101, FirstName = "Alice", LastName = "Johnson" },
+                    Cart = new Dictionary<ProductModel, int>
+                    {
+                        { new ProductModel { Id = 201, Title = "Product1", Price = 10.99 }, 2 },
+                        { new ProductModel { Id = 202, Title = "Product2", Price = 15.99 }, 1 }
+                    },
+                    TotalPrice = 37.97,
+                    NutriPointsReward = 20,
+                    Note = "Sample order 1"
+                },
+                new Order
+                {
+                    Id = 2,
+                    OrderDate = DateTime.Now.AddDays(-5),
+                    Customer = new Customer { Id = 102, FirstName = "Bob", LastName = "Smith" },
+                    Cart = new Dictionary<ProductModel, int>
+                    {
+                        { new ProductModel { Id = 203, Title = "Product3", Price = 8.50 }, 3 },
+                        { new ProductModel { Id = 204, Title = "Product4", Price = 12.75 }, 1 }
+                    },
+                    TotalPrice = 44.25,
+                    NutriPointsReward = 15,
+                    Note = "Sample order 2"
+                }
+            };
+
+        }
+
+        public bool CreateOrder(Order order)
+        {
+            order.Id = orders.Count + 1;
+            orders.Add(order);
+            return true;
+        }
+
+        public Dictionary<ProductModel, int> GetShoppingCart(int id)
+        {
+            Order order = orders.Find(o => o.Id == id);
+            return order?.Cart ?? new Dictionary<ProductModel, int>();
+        }
+
+        public Order GetOrderById(int id)
+        {
+            return orders.Find(o => o.Id == id) ?? new Order();
+        }
+
+        public List<Order> GetOrders()
+        {
+            return orders;
+        }
+
+        public int GetCustomerNutriPoints(int customerId)
+        {
+            return 50;
+        }
+
+    }
+}
---
+++ FitFusion/Controllers/MockDAO/MockUserDAO.cs
@@ -0,0 +1,159 @@
+﻿using Interfaces;
+using Models.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.MockDAO
+{
+    public class MockUserDAO : IUser
+    {
+        private readonly List<User> users;
+
+        public MockUserDAO()
+        {
+            users = new List<User>
+            {
+                new Owner
+                (
+                    id: 1,
+                    firstName: "John",
+                    lastName: "Doe",
+                    email: "john.doe@example.com",
+                    passwordHash: "hashed_password",
+                    passwordSalt: "salt",
+                    address: "123 Main St",
+                    phone: "123-456-7890"
+                ),
+                new Staff
+                (
+                    id: 2,
+                    firstName: "Jane",
+                    lastName: "Smith",
+                    email: "jane.smith@example.com",
+                    passwordHash: "hashed_password",
+                    passwordSalt: "salt",
+                    address: "456 Oak St",
+                    phone: "987-654-3210"
+                ),
+                new Customer
+                (
+                    id: 3,
+                    firstName: "Alice",
+                    lastName: "Johnson",
+                    email: "alice.johnson@example.com",
+                    passwordHash: "hashed_password",
+                    passwordSalt: "salt",
+                    address: "789 Pine St",
+                    loyaltyScore: 100
+                )
+            };
+        }
+
+        public bool IsEmailAlreadyExists(string email)
+        {
+            return users.Any(user => user.Email == email);
+        }
+
+
+        public User? AuthenticateUser(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CreateUser(User user)
+        {
+            if (!IsEmailAlreadyExists(user.Email))
+            {
+                users.Add(user);
+                return true;
+            } 
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteUser(User user)
+        {
+            if (!IsEmailAlreadyExists(user.Email))
+            {
+                users.Remove(user);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public User GetUserByEmail(string email)
+        {
+            User? user = users.FirstOrDefault(u => u.Email == email);
+
+            if (user != null)
+            {
+                return user;
+            }
+            else
+            {
+                throw new InvalidOperationException("User not found");
+            }
+        }
+
+        public User GetUserById(int id, User role)
+        {
+            User? user = users.FirstOrDefault(u => u.Id == id && u.GetType() == role.GetType());
+
+            if (user != null)
+            {
+                return user;
+            }
+            else
+            {
+                throw new InvalidOperationException("User not found");
+            }
+        }
+
+        public List<User> GetUsers(User role)
+        {
+            return users;
+        }
+
+        public bool UpdateUser(User user)
+        {
+            User? existingUser = users.FirstOrDefault(u => u.Id == user.Id);
+
+            if (existingUser != null)
+            {
+                existingUser.FirstName = user.FirstName;
+                existingUser.LastName = user.LastName;
+                existingUser.Email = user.Email;
+                existingUser.PasswordHash = user.PasswordHash;
+                existingUser.PasswordSalt = user.PasswordSalt;
+
+                if (user is Staff)
+                {
+                    ((Staff)existingUser).Phone = ((Staff)user).Phone;
+                }
+                else if (user is Owner)
+                {
+                    ((Owner)existingUser).Phone = ((Owner)user).Phone;
+                }
+                else if (user is Customer)
+                {
+                    ((Customer)existingUser).NutriPoints = ((Customer)user).NutriPoints;
+                }
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
