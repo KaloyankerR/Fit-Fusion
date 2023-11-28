84b7ac90916aa109c6112d911800d25e5509bd3c
Kaloyan <kaloyankulov2003kk@gmail.com>
2023-11-28 23:43:43 +0100

    Create page refactoring

--- FitFusion/FitFusionWeb/Pages/Users/Create.cshtml.cs
+++ FitFusion/FitFusionWeb/Pages/Users/Create.cshtml.cs
@@ -1,13 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Models.User;
+using System.ComponentModel.DataAnnotations;
 
 namespace FitFusionWeb.Pages.Users
 {
     public class CreateModel : PageModel
     {
-        
-        public User NewUser { get; set; }
+        public class UserCreateModel
+        {
+            public int Id { get; set; }
+
+            [Required(ErrorMessage = "First Name is required.")]
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+
+            [Required(ErrorMessage = "Last Name is required.")]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
+
+            [Required(ErrorMessage = "Email is required.")]
+            [EmailAddress(ErrorMessage = "Invalid Email Address.")]
+            public string Email { get; set; }
+
+            [Required(ErrorMessage = "Password is required.")]
+            [DataType(DataType.Password)]
+            public string PasswordHash { get; set; }
+
+            [Required(ErrorMessage = "Address is required.")]
+            public string Address { get; set; }
+        }
+
+        [BindProperty]
+        public UserCreateModel UserForm { get; set; } = new();
             
         public void OnGet()
         { }
